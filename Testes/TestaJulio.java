8c75c08b5d02a2411fa8d56bd2901c47809a84eb
Lucas <lucasbagb@gmail.com>
2017-03-09 14:15:00 -0300

    Criação de classes de teste parcial.

---
+++ Testes/TestaJulio.java
@@ -0,0 +1,15 @@
+package Testes;
+
+import JTextField.Cadastros.TelaCadCarros;
+
+public class TestaJulio {
+
+	public static void main(String[] args) {
+
+		TelaCadCarros tcc = new TelaCadCarros();
+
+		tcc.JTPCarros();
+
+	}
+
+}
---
+++ Testes/TestaLucas.java
@@ -0,0 +1,15 @@
+package Testes;
+
+import JTextField.TelaCadastros;
+
+public class TestaLucas {
+
+	public static void main(String[] args) {
+
+		TelaCadastros tc = new TelaCadastros();
+
+		tc.JTPCadastros();
+
+	}
+
+}
